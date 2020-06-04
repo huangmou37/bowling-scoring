ce88fa9b023064012cd72b47dc0fcd0642c85362
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-06-04 10:05:47 +0800

    [cheng] add a test case with 1 blow out in first bowling and 9 blow out in second bowling in the final round

--- bowling-scoring/src/test/java/cn/xpbootcamp/bowling/BowlingRoundTest.java
+++ bowling-scoring/src/test/java/cn/xpbootcamp/bowling/BowlingRoundTest.java
@@ -57,4 +57,13 @@ public class BowlingRoundTest {
 
     assertFalse(finalRound.isFinished());
   }
+
+  @Test
+  public void should_return_false_when_is_finished_given_10th_round_with_1st_1_and_2nd_9_blow_out() {
+    BowlingRound finalRound = new BowlingRound(10);
+    finalRound.blowOut(1);
+    finalRound.blowOut(9);
+
+    assertFalse(finalRound.isFinished());
+  }
 }
