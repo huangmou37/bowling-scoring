cf6590c3d31b256d01cd39172519d2f421d59cc8
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-06-04 09:52:49 +0800

    [cheng] return true when calling isFinished() given two bowling in a round

--- bowling-scoring/src/test/java/cn/xpbootcamp/bowling/BowlingRoundTest.java
+++ bowling-scoring/src/test/java/cn/xpbootcamp/bowling/BowlingRoundTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertTrue;
 public class BowlingRoundTest {
 
   @Test
-  public void should_return_false_when_is_finished_given_1st_4_blow_out() {
+  public void should_return_false_when_is_finished_given_a_round_with_1st_4_blow_out() {
     BowlingRound firstRound = new BowlingRound();
     firstRound.blowOut(4);
 
@@ -16,9 +16,27 @@ public class BowlingRoundTest {
   }
 
   @Test
-  public void should_return_true_when_is_finished_given_1st_10_blow_out() {
+  public void should_return_true_when_is_finished_given_a_round_with_1st_10_blow_out() {
     BowlingRound firstRound = new BowlingRound();
     firstRound.blowOut(10);
+
+    assertTrue(firstRound.isFinished());
+  }
+
+  @Test
+  public void should_return_false_when_is_finished_given_a_round_with_1st_4_and_2nd_5_blow_out() {
+    BowlingRound firstRound = new BowlingRound();
+    firstRound.blowOut(4);
+    firstRound.blowOut(5);
+
+    assertTrue(firstRound.isFinished());
+  }
+
+  @Test
+  public void should_return_false_when_is_finished_given_a_round_with_1st_4_and_2nd_6_blow_out() {
+    BowlingRound firstRound = new BowlingRound();
+    firstRound.blowOut(4);
+    firstRound.blowOut(6);
 
     assertTrue(firstRound.isFinished());
   }
